ad58fe6081411eb59175135345882884889004b9
choihyunwoo1 <a0058765@gmail.com>
2025-11-27 18:55:01 +0900

    Slash

--- Assets/_Sample/13CharacterLevelup/LevelUp.cs
+++ Assets/_Sample/13CharacterLevelup/LevelUp.cs
@@ -4,65 +4,68 @@ using System.Collections;
 
 namespace MyVfx
 {
-    public class LevelUp : MonoBehaviour
+    public class Levelup : MonoBehaviour
     {
-        #region Varialbes
+        #region Variables
         //참조
-        Animator animator;
+        private Animator animator;
 
-        bool isLevelUp = false; //레벨업 이펙트 플레이 체크
+        public VisualEffect levelup;    //레벨업 vfx 이펙트
+        public Renderer bodyRenderer;   //메터리얼을 관리하는 랜더러
+        public Material glowMaterial;   //레벨업하는 동안 바꿀 메터리얼
 
-        public VisualEffect levelUp; //레벨업 VFX 이펙트
-        public Renderer bodyRenderer; //메터리얼을 관리하는 렌더러
-        public Material blowMaterial; //바꿀 메터리얼
-        Material originMaterial; //원래 메터리얼
-        string paramLevelUp = "Levelup";
+        private Material originMaterial;    //원래 메터리얼
+
+        private bool isLevelup = false;     //레벨업 이펙트 플레이 체크
+
+        private const string ParamLevelup = "Levelup";
         #endregion
 
         #region Unity Event Method
         private void Awake()
         {
             //참조
-             animator = GetComponent<Animator>();
+            animator = GetComponent<Animator>();
         }
+
         private void Start()
         {
             //초기화
-            isLevelUp = false;
-            originMaterial = bodyRenderer.material; 
-            
+            isLevelup = false;
+            originMaterial = bodyRenderer.material;
         }
+
         private void Update()
         {
             //마우스 우클릭하면 레벨업 치팅
-            if (Input.GetMouseButtonDown(1)&& isLevelUp == false)
+            if(Input.GetMouseButtonDown(1) && isLevelup == false)
             {
-                StartCoroutine(LevelupEffect());    
+                StartCoroutine(LevelupEffect());
             }
         }
         #endregion
 
         #region Custom Method
         IEnumerator LevelupEffect()
-        { 
-            isLevelUp =true;
+        {
+            isLevelup = true;
 
-            //레벨업 애니 및 vfx이펙트 시작
-            animator.SetTrigger(paramLevelUp);
-            levelUp.gameObject.SetActive(true);
-
+            //레벨업 애니 및 vfx 이펙트 시작
+            animator.SetTrigger(ParamLevelup);
+            levelup.gameObject.SetActive(true);
+            
             yield return new WaitForSeconds(0.2f);
 
             //메터리얼 바꿔치기
-            bodyRenderer.material = blowMaterial;
+            bodyRenderer.material = glowMaterial;
             yield return new WaitForSeconds(0.8f);
 
             bodyRenderer.material = originMaterial;
             yield return new WaitForSeconds(2f);
 
             //레벨업 이펙트 초기화
-            levelUp.gameObject.SetActive(false);
-            isLevelUp =false;
+            levelup.gameObject.SetActive(false);
+            isLevelup = false;
         }
         #endregion
     }
